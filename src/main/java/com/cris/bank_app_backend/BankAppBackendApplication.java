799b2ac91538fc20ef18ca894249e7792bdbb629
fcr1s <cristobal.floresa@usach.cl>
2024-10-26 15:35:19 -0300

    update

--- src/main/java/com/cris/bank_app_backend/BankAppBackendApplication.java
+++ src/main/java/com/cris/bank_app_backend/BankAppBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankAppBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankAppBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankAppBackendApplication.class, args);
+    }
 
 }
--- src/test/java/com/cris/bank_app_backend/BankAppBackendApplicationTests.java
+++ src/test/java/com/cris/bank_app_backend/BankAppBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankAppBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
