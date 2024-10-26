3a3c3640a105624311b06759b7182c2c942d226d
fcr1s <cristobal.floresa@usach.cl>
2024-10-26 15:32:21 -0300

    Initial commit

---
+++ src/main/java/com/cris/bank_app_backend/BankAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cris.bank_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cris/bank_app_backend/BankAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cris.bank_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
